76a05400581308e29cafb9b36df939176a2cb917
Tallis Souza <tallisfagundes@gmail.com>
2022-12-07 01:28:05 -0300

    Implement simulation calculation logic

--- src/app/components/simulator/simulator.component.ts
+++ src/app/components/simulator/simulator.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./simulator.component.scss']
 })
 export class SimulatorComponent implements OnInit {
+  selectedMonth: Date = new Date();
+  amount = 0;
+  monthAmount = 1;
+  monthlyAmount = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.selectedMonth.setMonth(this.selectedMonth.getMonth() + 1);
   }
 
+  updateSelectedMonth(date: Date): void {
+    this.selectedMonth = new Date(
+      date.getFullYear(),
+      date.getMonth(),
+      date.getDate()
+    );
+    this.updateSimulation();
+  }
+
+  updateSimulation(): void {
+    const today = new Date();
+    const yearDifference = this.selectedMonth.getFullYear() - today.getFullYear();
+    this.monthAmount = (yearDifference * 12) - (today.getMonth() - this.selectedMonth.getMonth());
+    this.monthlyAmount = this.amount / this.monthAmount;
+  }
 }
