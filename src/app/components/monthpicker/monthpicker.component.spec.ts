c0b2b840b7856aecaf400dd2d6f10c7d95de3ed3
Tallis Souza <tallisfagundes@gmail.com>
2022-12-07 03:04:40 -0300

    Add some unit tests

--- src/app/components/monthpicker/monthpicker.component.spec.ts
+++ src/app/components/monthpicker/monthpicker.component.spec.ts
@@ -1,3 +1,4 @@
+import { NO_ERRORS_SCHEMA } from '@angular/compiler';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MonthpickerComponent } from './monthpicker.component';
@@ -8,7 +9,8 @@ describe('MonthpickerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MonthpickerComponent ]
+      declarations: [ MonthpickerComponent ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
 
@@ -19,5 +21,54 @@ describe('MonthpickerComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('goToNextMonth', () => {
+    it('should update month value', () => {
+      component.month.setMonth(1);
+      component.goToNextMonth();
+      expect(component.month.getMonth()).toBe(2);
+    });
+    it('should set canGoToPreviousMonth flag to true', () => {
+      component.month.setMonth(1);
+      component.canGoToPreviousMonth = false;
+      component.goToNextMonth();
+      expect(component.canGoToPreviousMonth).toBe(true);
+    });
+    it('should emit event with new month value', () => {
+      component.month.setMonth(1);
+      spyOn(component.monthChanged, 'emit');
+      component.goToNextMonth();
+      expect(component.monthChanged.emit).toHaveBeenCalledWith(component.month);
+    });
+  });
+
+  describe('goToPreviousMonth', () => {
+    it('should update month value', () => {
+      component.canGoToPreviousMonth = true;
+      component.month.setMonth(3);
+      component.goToPreviousMonth();
+      expect(component.month.getMonth()).toBe(2);
+    });
+    it('should set canGoToPreviousMonth flag to false', () => {
+      component.initialDate.setMonth(0)
+      component.canGoToPreviousMonth = true;
+      component.month.setMonth(1);
+      component.goToPreviousMonth();
+      expect(component.canGoToPreviousMonth).toBe(false);
+    });
+    it('should emit event with new month value', () => {
+      component.canGoToPreviousMonth = true;
+      component.month.setMonth(1);
+      spyOn(component.monthChanged, 'emit');
+      component.goToPreviousMonth();
+      expect(component.monthChanged.emit).toHaveBeenCalledWith(component.month);
+    });
+    it('should do nothing if canGoToPreviousMonth flag is false', () => {
+      component.canGoToPreviousMonth = false;
+      component.goToPreviousMonth();
+      spyOn(component.monthChanged, 'emit');
+      expect(component.monthChanged.emit).toHaveBeenCalledTimes(0);
+    });
   });
 });
--- src/app/components/simulator/simulator.component.spec.ts
+++ src/app/components/simulator/simulator.component.spec.ts
@@ -1,3 +1,4 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SimulatorComponent } from './simulator.component';
@@ -8,7 +9,8 @@ describe('SimulatorComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SimulatorComponent ]
+      declarations: [ SimulatorComponent ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
 
@@ -19,5 +21,18 @@ describe('SimulatorComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('updateSimulation', () => {
+    it('should match all the values', () => {
+      jasmine.clock().install();
+      jasmine.clock().mockDate(new Date('2022-1-1'));
+      component.selectedMonth.setMonth(6);
+      component.selectedMonth.setFullYear(2025);
+      component.amount = 42000;
+      component.updateSimulation();
+      expect(component.monthAmount).toBe(42);
+      expect(component.monthlyAmount).toBe(1000);
+    });
   });
 });
